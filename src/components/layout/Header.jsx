99cf373d097c36a944b426a2061b0f66488bcb0f
Betsy <132311670+betsymao@users.noreply.github.com>
2024-05-09 17:55:26 +1000

    responsive nav

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,26 +1,50 @@
+import { useState } from 'react';
+
 import { Link } from "react-router-dom";
 import { HashLink } from 'react-router-hash-link';
 import { FaBarsStaggered } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [toggleOpen, setToggleOpen] = useState(false);
   
   return (
     <>
       <header>
         <div className="container">
+          <nav> {/* flex */}
+            <div>
+              <Link to="/" className="logo">Betsy Mao</Link>
+            </div>
+
+            <div className="toggle" onClick={() => setToggleOpen(!toggleOpen)}>
+              <button className="toggle__btn"><FaBarsStaggered /></button>
+            </div>
+
+            <ul className={toggleOpen ? "open" : ""}>
+              <li>
+                <HashLink to="#works">Works</HashLink>
+              </li>
+              <li>
+                <HashLink to="#contact">Contact</HashLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {/* <header>
+        <div className="container">
           
           <div>
             <Link to="/" className="logo">Betsy Mao</Link>
-          </div>
+          </div> */}
 
           {/* <div className="toggle"> */}
             {/* <button><FaTimes /></button> */}
-            <button className="toggle__btn"><FaBarsStaggered /></button>
+            {/* <button className="toggle__btn"><FaBarsStaggered /></button> */}
           {/* </div> */}
 
-          <nav>
+          {/* <nav>
             <ul>
               <div className="menu">
                 <li>
@@ -33,7 +57,7 @@ function Header() {
             </ul>
           </nav>
         </div>
-      </header>
+      </header> */}
     </>
   )
 }
